fecba9c91e6b4b83dee434fee9ec86bee5061e9a
Nicholas Danylenko <nick23dan@gmail.com>
2020-10-27 23:45:03 -0400

    part a complete, found the distance between points

--- 06-math/distance.java
+++ 06-math/distance.java
@@ -2,25 +2,33 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    System.out.print("I will calculate the distance between 2 points.");
-    System.out.print("what is the x coordinate of your first point? ");
-    int x1 = in.nextInt();
+    System.out.println("I will calculate the distance between 2 points.");
 
-    System.out.print("what is the y coordinate of your first point? ");
-    int y1 = in.nextInt();
+    System.out.println("what is your first point: ");
+    String point1 = in.nextLine();
 
-    System.out.print("what is the x coordinate of your second point? ");
-    int x2 = in.nextInt();
-
-    System.out.print("what is the y coordinate of your second point? ");
-    int y2 = in.nextInt();
-
-    double subx = x1 - x2;
-    double suby = y1 - y2;
-
-    double distance = Math.sqrt(Math.pow(subx, 2) + Math.pow(suby, 2));
-    System.out.print(distance);
+    int comma = point1.indexOf(",");
+    int index1 = point1.lastIndexOf(")");
+    String x1 = point1.substring(1, comma);
+    String y1 = point1.substring(comma + 1, index1);
 
 
+    System.out.println("what is your second point: ");
+    String point2 = in.nextLine();
+
+    int comma2 = point2.indexOf(",");
+    int index2 = point2.lastIndexOf(")");
+    String x2 = point2.substring(1, comma2);
+    String y2 = point2.substring(comma2 + 1, index2);
+
+    int xnum = Integer.parseInt(x1);
+    int ynum = Integer.parseInt(y1);
+    int x2num = Integer.parseInt(x2);
+    int y2num = Integer.parseInt(y2);
+    int subx = xnum - x2num;
+    int suby = ynum - y2num;
+
+    int distance = (int) Math.sqrt(Math.pow(subx, 2) + Math.pow(suby, 2));
+    System.out.print("this is the distance betweent the points: " + distance);
   }
 }
